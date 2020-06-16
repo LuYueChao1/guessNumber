87b188e8980771619dc58b9ec4e46960fbdabd6d
unknown <1714407443@qq.com>
2020-06-16 21:37:40 +0800

    finish should_return_1A0B_given_when_input_actualAnswer_1358_and_inputAnswer_1746

--- src/test/java/com/twschool/practice/AnswerTest.java
+++ src/test/java/com/twschool/practice/AnswerTest.java
@@ -29,5 +29,11 @@ public class AnswerTest {
         String answerResult=answer.getAnswerResult("1358","1546");
         assertEquals("1A1B",answerResult);
     }
+    @Test
+    public void should_return_1A0B_given_when_input_actualAnswer_1358_and_inputAnswer_1746(){
+        Answer answer=new Answer();
+        String answerResult=answer.getAnswerResult("1358","1546");
+        assertEquals("1A1B",answerResult);
+    }
 
 }
